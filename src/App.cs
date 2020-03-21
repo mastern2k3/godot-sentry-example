f6c49aecd55ed2dc94bb7af05523b411654075a8
Nitzan Zada <nitzan.zada@gmail.com>
2020-03-22 00:33:28 +0200

    with exception buttons and everything

--- src/App.cs
+++ src/App.cs
@@ -8,7 +8,14 @@ using Sentry;
 public class App : Node2D {
 
 	public override void _Ready() {
-
 		SentrySdk.CaptureMessage("App Ready!");
+	}
+
+	private void _on_AwesomeButton_pressed() {
+		SentrySdk.AddBreadcrumb("Awesome button clicked");
+	}
+
+	private void _on_CrashButton_pressed() {
+		throw new Exception("Oh lord jesus it's a fire!");
 	}
 }
--- src/SentryController.cs
+++ src/SentryController.cs
@@ -14,8 +14,6 @@ public class SentryController : Node {
 			o.Dsn = new Dsn("https://451cf9b0fd9e4f0294a0d15b6c36bce2@sentry.io/5170198");
 		});
 
-		// AppDomain.CurrentDomain.UnhandledException += _UnhandledException;
-
 		SentrySdk.ConfigureScope(s => {
 			s.SetTag("os.name", OS.GetName());
 			s.SetTag("os.model", OS.GetModelName());
@@ -27,19 +25,10 @@ public class SentryController : Node {
 			level: BreadcrumbLevel.Info);
 	}
 
-	private void _UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+	public override void _Notification(int what) {
 
-		var ex = e.ExceptionObject as Exception;
-		if (ex == null) {
-
-			SentrySdk.CaptureMessage(
-				$"Unknown unhandled exception of type `{e.ExceptionObject.GetType()}`: {e.ExceptionObject}",
-				SentryLevel.Error);
-			return;
+		if (what == MainLoop.NotificationWmQuitRequest) {
+			SentrySdk.CaptureMessage("App Quit!");
 		}
-
-		GD.PrintErr(ex.ToString());
-
-		SentrySdk.CaptureException(ex);
 	}
 }
